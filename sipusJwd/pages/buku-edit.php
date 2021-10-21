d894df26c0f2aae690feb38320ba0d4920dab656
LAPTOP-50JIFB1T\YovitaMargaret <yovitaabigail2013@gmail.com>
2021-10-21 18:02:06 +0700

    Update buku-edit.php

--- sipusJwd/pages/buku-edit.php
+++ sipusJwd/pages/buku-edit.php
@@ -4,35 +4,37 @@
 	$r_tampil_buku=mysqli_fetch_array($q_tampil_buku);
 
 ?>
-<div id="label-page"><h3>Edit Data Buku</h3></div>
+<div class="judul"><h3>Edit Data Buku</h3></div>
 <div id="content">
-	<form action="proses/buku-edit-proses.php" method="post">
-	<table id="tabel-input">
-		<tr>
-			<td class="label-formulir">ID Buku</td>
-			<td class="isian-formulir"><input type="text" name="id_buku" value="<?php echo $r_tampil_buku['idbuku']; ?>" readonly="readonly" class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Judul Buku</td>
-			<td class="isian-formulir"><input type="text" name="judul_buku" id="judul_buku" value="<?php echo $r_tampil_buku['judulbuku']; ?>" class="isian-formulir isian-formulir-border"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Kategori</td>
-			<td class="isian-formulir"><input type="text" name="kategori" id="kategori" value="<?php echo $r_tampil_buku['kategori']; ?>" class="isian-formulir isian-formulir-border"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Pengarang</td>
-			<td class="isian-formulir"><input type="text" name="pengarang" id="pengarang" value="<?php echo $r_tampil_buku['pengarang']; ?>" class="isian-formulir isian-formulir-border"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir">Penerbit</td>
-			<td class="isian-formulir"><input type="text" name="penerbit" id="penerbit" value="<?php echo $r_tampil_buku['penerbit']; ?>" class="isian-formulir isian-formulir-border"></td>
-		</tr>
-		<tr>
-			<td class="label-formulir"></td>
-			<td class="isian-formulir"><input type="submit" name="simpan" value="Simpan" id="tombol-simpan"></td>
-		</tr>
-	</table>
+	<form action="proses/buku-edit-proses.php" method="POST" onsubmit= "return validasi()">
+	<div class="form-group">
+		<label for="">ID Buku</label>
+		<input type="text" name="id_buku" class="form-control" value="<?php echo $r_tampil_buku['idbuku']; ?>" readonly>
+	</div>
+
+	<div class="form-group">
+		<label for="">Judul Buku</label>
+		<input type="text" name="judul_buku" id="judul_buku"class="form-control" value="<?php echo $r_tampil_buku['judulbuku']; ?>">
+	</div>
+
+	<div class="form-group">
+		<label for="">Kategori</label>
+		<input type="text" name="kategori" id="kategori" value="<?php echo $r_tampil_buku['kategori']; ?>" class="isian-formulir isian-formulir-border">
+	</div>
+
+	<div class="form-group">
+		<label for="">Pengarang</label>
+		<input type="text" name="pengarang" id="pengarang" value="<?php echo $r_tampil_buku['pengarang']; ?>" class="isian-formulir isian-formulir-border">
+	</div>
+
+	<div class="form-group">
+		<label for="">Penerbit</label>
+		<input type="text" name="penerbit" id="penerbit" value="<?php echo $r_tampil_buku['penerbit']; ?>" class="isian-formulir isian-formulir-border">
+	</div>
+	<br>
+	<div>
+	<input type="submit" name="simpan" value="Simpan" class="btn btn-primary">
+</div>
 	</form>
 </div>
 
@@ -43,30 +45,29 @@
 		pengarang = document.getElementById("pengarang");
 		penerbit = document.getElementById("penerbit");
 	
-	if (judul_buku.value == '') {
+	if (judul_buku.value == ''  ) {
 		alert('Mohon isi data dengan lengkap !');
 		judul_buku.focus();
 			return false;
 		}
-	}
-
+	
 	if (kategori.value == '') {
 		alert('Mohon isi data dengan lengkap !');
 		kategori.focus();
 			return false;
 		}
-	}if (pengarang.value == '') {
+	if (pengarang.value == '') {
 		alert('Mohon isi data dengan lengkap !');
 		pengarang.focus();
 			return false;
 		}
-	}if (penerbit.value == '') {
+	if (penerbit.value == '') {
 		alert('Mohon isi data dengan lengkap !');
 		penerbit.focus();
 			return false;
 		}
-	}
-
+	
+}
 
 
 </script>
